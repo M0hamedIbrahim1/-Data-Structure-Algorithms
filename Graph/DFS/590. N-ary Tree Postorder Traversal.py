7d75129f6036a0810cd65dacdb3cec0893d8349f
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2023-03-03 11:43:53 +0200

    Create 590. N-ary Tree Postorder Traversal.py

---
+++ Graph/DFS/590. N-ary Tree Postorder Traversal.py
@@ -0,0 +1,25 @@
+# link   : https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+# author : Mohamed Ibrahim
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        def dfs(root):
+            if not root:
+                return
+            for i in root.children:
+                dfs(i)
+            res.append(root.val)
+        res = []
+        dfs(root)
+        return res
+        
+        
