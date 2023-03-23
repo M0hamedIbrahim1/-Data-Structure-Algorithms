07a38745a02606830580fa519c8005daef4ce41c
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2023-03-23 16:55:04 +0200

    Create 310. Minimum Height Trees.py

---
+++ Graph/Problems/310. Minimum Height Trees.py
@@ -0,0 +1,25 @@
+# link   : https://leetcode.com/problems/minimum-height-trees/description/
+# author : Mohamed Ibrahim
+
+
+def findMinHeightTrees(self, n, edges):
+    if n == 1: return [0] 
+    adj = [set() for _ in xrange(n)]
+    for i, j in edges:
+        adj[i].add(j)
+        adj[j].add(i)
+
+    leaves = [i for i in xrange(n) if len(adj[i]) == 1]
+
+    while n > 2:
+        n -= len(leaves)
+        newLeaves = []
+        for i in leaves:
+            j = adj[i].pop()
+            adj[j].remove(i)
+            if len(adj[j]) == 1: newLeaves.append(j)
+        leaves = newLeaves
+    return leaves
+	
+
+
