78cd6e0d74936f699be397697287ffa76eaedb6c
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2023-07-30 10:20:06 +0300

    Update 2799. Count Complete Subarrays in an Array.py

--- Sliding Window/Problems/2799. Count Complete Subarrays in an Array.py
+++ Sliding Window/Problems/2799. Count Complete Subarrays in an Array.py
@@ -13,3 +13,27 @@ class Solution:
                 if(len(seen)==c):
                     res+=1
         return res
+
+# Sliding Window
+
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        l,r = 0 , 0
+        cnt , ln = 0 , len(set(nums))
+        n =  len(nums)
+        d = defaultdict(int)
+
+        while r < n:
+            d[nums[r]]+=1
+
+            while len(d) == ln:
+                cnt+= n - r
+
+                d[nums[l]] -= 1
+                if d[nums[l]] == 0:
+                    del d[nums[l]]
+                
+                l+=1
+            r+=1
+        return cnt
+        
