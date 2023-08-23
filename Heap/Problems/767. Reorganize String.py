e366d709d2c881a84e4c717dd7544d759ab4781e
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2023-08-23 20:43:37 +0300

    Create 767. Reorganize String.py

---
+++ Heap/Problems/767. Reorganize String.py
@@ -0,0 +1,25 @@
+# link : https://leetcode.com/problems/reorganize-string/
+# author : Mohamed Ibrahim
+
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        ans = []
+
+        pq = [(-count, char) for char, count in Counter(s).items()]
+        heapify(pq)
+
+        while pq:
+            count_first, char_first = heappop(pq)
+            if not ans or char_first != ans[-1]:
+                ans.append(char_first)
+                if count_first + 1 != 0: 
+                    heappush(pq, (count_first + 1, char_first))
+            else:
+                if not pq: return ''
+                count_second, char_second = heappop(pq)
+                ans.append(char_second)
+                if count_second + 1 != 0:
+                    heappush(pq, (count_second + 1, char_second))
+                heappush(pq, (count_first, char_first))
+
+        return ''.join(ans)
